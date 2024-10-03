abfeb5ea9bcfc804093b3b7db8ac71f65b1081e7
mauryakhushiramjeet <mauryakushi004@gmail.com>
2024-10-03 11:28:37 +0530

    Exploring Post,Get,Use Api call

--- reademe.md
+++ reademe.md
@@ -8,3 +8,6 @@ Install nodemon and update scripts inside package.json
 What are dependencies
 What is the use of "-g" while npm install
 Difference between caret and tilde (^ vs)
+play with routes ex: /hello,"/"
+Order of routes is Matter
+---install Postman app and mae a workspace/collection >test API call
--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express=require("express")
 const app=express()
-app.use((req,res)=>{
-    res.send("hellow from the  ")
+app.get("/hello",(req,res)=>{
+    res.send("hellow from the hello  ")
 })
-app.listen(5050,()=>{
+app.post("/",(req,res)=>{
+    res.send({firstName:"Anit Maurya",age:"22"})
+})
+
+app.use("/namste",(req,res)=>{
+    res.send("hellow from the namste  ")
+})
+app.listen(5050,()=>{ 
     console.log("server is created on port 5050....")
 })
