45d8b787903542685f032feffd33d5d6bb4fb811
mauryakhushiramjeet <mauryakushi004@gmail.com>
2024-10-03 09:51:25 +0530

    secoond

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const app=express()
-app.use((req,res)=>{
-    res.send("hellow from the server ")
+app.use("/hellow",(req,res)=>{
+    res.send("hellow from the  ")
 })
 app.listen(5050,()=>{
     console.log("server is created on port 5050....")
